9948efad59cdb3e29e77fe5ec8772eb743c9eddb
Alfonsojedi <150245030+Alfonsojedi@users.noreply.github.com>
2024-01-29 09:11:10 +0100

    Update App.jsx

--- 02-pokemon/src/App.jsx
+++ 02-pokemon/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
 
@@ -9,6 +7,7 @@ function App() {
 
   return (
     <>
+      <PokeNav/>
       <div className="card">
         <div class="card-header">
           <div className='row'>
