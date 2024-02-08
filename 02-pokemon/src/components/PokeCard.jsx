d4e573b9a753b3431e098768dd14b5a45dbdae78
Alfonsojedi <alfonsoeljedi@gmail.com>
2024-02-08 21:52:33 +0100

    modified:   src/components/PokeCard.jsx 	modified:   src/components/ProgressBar.jsx

--- 02-pokemon/src/components/PokeCard.jsx
+++ 02-pokemon/src/components/PokeCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import ProgressBar from './ProgressBar'
 
 export function PokeCard(){
   const pokemon = {
@@ -10,28 +11,10 @@ export function PokeCard(){
     spdfs: "65",
     spd: "105"
   }
-  const styleHP = {
-    width: eval(100*pokemon.hp/255)+"%"
-  }
-  const styleAtk = {
-    width: eval(100*pokemon.atk/255)+"%"
-  }
-  const styleDfs = {
-    width: eval(100*pokemon.dfs/255)+"%"
-  }
-  const styleSpAtk = {
-    width: eval(100*pokemon.spatk/255)+"%"
-  }
-  const styleSpDfs = {
-    width: eval(100*pokemon.spdfs/255)+"%"
-  }
-  const styleSpd = {
-    width: eval(100*pokemon.spd/255)+"%"
-  }
   return(
     <div className='col-4 p-2'>
         <div className="card">
-            <div class="card-header">
+            <div className='card-header'>
             <div className='row'>
                 <div className='col type fire'>
                     Fire
@@ -41,57 +24,15 @@ export function PokeCard(){
                 </div>
             </div>
             </div>
-            <div class="card-main">
-            <h5 class="card-title">{pokemon.name}</h5>
+            <div className='card-main'>
+            <h5 className='card-title'>{pokemon.name}</h5>
             <div className='p-1'>
-                <div class="row">
-                    <div className='col-4 col-md-3 mini'>Hp</div>
-                    <div class="col-8 col-md-9">
-                        <div class="progress" role="progressbar" aria-label="HP" aria-valuenow="100" aria-valuemin="0" aria-valuemax="255">
-                            <div className='progress-bar fairy' style={styleHP}>{pokemon.hp}</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div className='col-4 col-md-3 mini'>Atk</div>
-                    <div class="col-8 col-md-9">
-                        <div class="progress" role="progressbar" aria-label="HP" aria-valuenow="100" aria-valuemin="0" aria-valuemax="255">
-                            <div className='progress-bar fire' style={styleAtk}>{pokemon.atk}</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div className='col-4 col-md-3 mini'>Dfs</div>
-                    <div class="col-8 col-md-9">
-                        <div class="progress" role="progressbar" aria-label="HP" aria-valuenow="100" aria-valuemin="0" aria-valuemax="255">
-                            <div className='progress-bar flying' style={styleDfs}>{pokemon.dfs}</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div className='col-4 col-md-3 mini'>SpAtk</div>
-                    <div class="col-8 col-md-9">
-                        <div class="progress" role="progressbar" aria-label="HP" aria-valuenow="100" aria-valuemin="0" aria-valuemax="255">
-                            <div className='progress-bar fighting' style={styleSpAtk}>{pokemon.spatk}</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div className='col-4 col-md-3 mini'>SpDfs</div>
-                    <div class="col-8 col-md-9">
-                        <div class="progress" role="progressbar" aria-label="HP" aria-valuenow="100" aria-valuemin="0" aria-valuemax="255">
-                            <div className='progress-bar water' style={styleSpDfs}>{pokemon.spdfs}</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div className='col-4 col-md-3 mini'>Spd</div>
-                    <div class="col-8 col-md-9">
-                        <div class="progress" role="progressbar" aria-label="HP" aria-valuenow="100" aria-valuemin="0" aria-valuemax="255">
-                            <div className='progress-bar bug' style={styleSpd}>{pokemon.spd}</div>
-                        </div>
-                    </div>
-                </div>
+                <ProgressBar amount={pokemon.hp} stat={"HP"}></ProgressBar>
+                <ProgressBar amount={pokemon.atk} stat={"Atk"}></ProgressBar>
+                <ProgressBar amount={pokemon.dfs} stat={"Dfs"}></ProgressBar>
+                <ProgressBar amount={pokemon.spatk} stat={"SpAtk"}></ProgressBar>
+                <ProgressBar amount={pokemon.spdfs} stat={"SpDfs"}></ProgressBar>
+                <ProgressBar amount={pokemon.spd} stat={"Spd"}></ProgressBar>
             </div>
             <a href="#" class="btn btn-primary">See {pokemon.name}</a>
             </div>
--- 02-pokemon/src/components/ProgressBar.jsx
+++ 02-pokemon/src/components/ProgressBar.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 
-export function ProgressBar(stat){
+export function ProgressBar({amount,stat}){
+    
+    const styleAmount = {
+        width: eval(100*amount/255)+"%",
+        background: (amount>100 ? "#00CC00":"#CC8800")
+    }
     return(
-        <div class="row">
-            <div class="col-3">{stat}</div>
-            <div class="col-9">
+        <div class="row mb-1">
+            <div className='col-4 col-md-3 mini'>{stat}</div>
+            <div class="col-8 col-md-9">
                 <div class="progress" role="progressbar" aria-label={stat} aria-valuenow="100" aria-valuemin="0" aria-valuemax="255">
-                    <div className='progress-bar fairy' style={styleHP}>{pokemon.hp}</div>
+                    <div className='progress-bar' style={styleAmount}>{amount}</div>
                 </div>
             </div>
         </div>
