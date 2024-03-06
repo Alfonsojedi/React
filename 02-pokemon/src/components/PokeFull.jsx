3de391a01986b0c857518c1c0573bd21f3d83758
Alfonsojedi <alfonsojfernandezcastillo@gmail.com>
2024-03-06 09:31:41 +0100

    modified:   02-pokemon/src/components/PokeFull.jsx

--- 02-pokemon/src/components/PokeFull.jsx
+++ 02-pokemon/src/components/PokeFull.jsx
@@ -46,6 +46,7 @@ export function PokeFull({pokemon}){
                         <div className="row mt-1">
                             <div className="col-5">
                                 <div className="watercard radius aling center p-1">
+                                    <h4 className="aligntext ">{pokemon.species.name.toUpperCase()}</h4>
                                     <img className='w-100' src={pokemon.sprites.front_default}></img>
                                     <div className='row aling center'>
                                         <div className="col-5 mb-1">Altura: <span className="kindatrans">{pokemon.height/10}</span>m</div>
@@ -60,27 +61,26 @@ export function PokeFull({pokemon}){
                             </div>
                             <div className="col-7">
                                 <div className={"radius p-1 "+color1+"card"}>
-                                <h4>{pokemon.species.name.toUpperCase()}</h4>
-                                <h6>Habilidades:</h6>
-                                <ol>
-                                    <li>{pokemon.abilities[0]["ability"]["name"]}</li>
-                                    {skill1 ?
-                                    <li>{pokemon.abilities[1]["ability"]["name"]}</li>:<></>}
-                                    {skill2 ?
-                                    <li>{pokemon.abilities[2]["ability"]["name"]}</li>:<></>}
-                                </ol>
-                                <ProgressBar amount={pokemon.stats[0]["base_stat"]} stat={"HP"}></ProgressBar>
-                                <ProgressBar amount={pokemon.stats[1]["base_stat"]} stat={"Atk"}></ProgressBar>
-                                <ProgressBar amount={pokemon.stats[2]["base_stat"]} stat={"Dfs"}></ProgressBar>
-                                <ProgressBar amount={pokemon.stats[3]["base_stat"]} stat={"SpAtk"}></ProgressBar>
-                                <ProgressBar amount={pokemon.stats[4]["base_stat"]} stat={"SpDfs"}></ProgressBar>
-                                <ProgressBar amount={pokemon.stats[5]["base_stat"]} stat={"Spd"}></ProgressBar>
-                                <p>Estadísticas totales: {total} puntos</p>
-                                <h6>Cadena evolutiva:</h6>
-                                <p className="kindatrans">Bulvasaur||Ivysaur||Venosaur</p>
-                                <p className="kindatrans">Bulvasaur||Snorlax</p>
-                                <h6>Zona de captura:</h6>
-                                <p className="kindatrans">{pokemon.location_area_encounters}</p>
+                                    <h6>Habilidades:</h6>
+                                    <ol>
+                                        <li>{pokemon.abilities[0]["ability"]["name"]}</li>
+                                        {skill1 ?
+                                        <li>{pokemon.abilities[1]["ability"]["name"]}</li>:<></>}
+                                        {skill2 ?
+                                        <li>{pokemon.abilities[2]["ability"]["name"]}</li>:<></>}
+                                    </ol>
+                                    <ProgressBar amount={pokemon.stats[0]["base_stat"]} stat={"HP"}></ProgressBar>
+                                    <ProgressBar amount={pokemon.stats[1]["base_stat"]} stat={"Atk"}></ProgressBar>
+                                    <ProgressBar amount={pokemon.stats[2]["base_stat"]} stat={"Dfs"}></ProgressBar>
+                                    <ProgressBar amount={pokemon.stats[3]["base_stat"]} stat={"SpAtk"}></ProgressBar>
+                                    <ProgressBar amount={pokemon.stats[4]["base_stat"]} stat={"SpDfs"}></ProgressBar>
+                                    <ProgressBar amount={pokemon.stats[5]["base_stat"]} stat={"Spd"}></ProgressBar>
+                                    <p>Estadísticas totales: {total} puntos</p>
+                                    <h6>Cadena evolutiva:</h6>
+                                    <p className="kindatrans">Bulvasaur||Ivysaur||Venosaur</p>
+                                    <p className="kindatrans">Bulvasaur||Snorlax</p>
+                                    <h6>Zona de captura:</h6>
+                                    <p className="kindatrans">{pokemon.location_area_encounters}</p>
                                 </div>
                             </div>
                         </div>
