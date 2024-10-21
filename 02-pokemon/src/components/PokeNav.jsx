ea13e1833be3930687978ee85952068b34a69181
Alfonsojedi <150245030+Alfonsojedi@users.noreply.github.com>
2024-10-21 14:28:58 +0200

    Update PokeNav.jsx

--- 02-pokemon/src/components/PokeNav.jsx
+++ 02-pokemon/src/components/PokeNav.jsx
@@ -4,12 +4,12 @@ import NavItem from './NavItem'
 export function PokeNav(){
 
   return(
-    <nav class="navbar navbar-expand-sm bg-danger">
-      <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <nav className="navbar navbar-expand-sm bg-danger">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <NavItem region={"Kanto"}></NavItem>
             <NavItem region={"Johto"}></NavItem>
