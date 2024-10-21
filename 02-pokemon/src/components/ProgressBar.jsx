cb1a6aecc77f15d2856652bd99fee615c2fbe148
Alfonsojedi <150245030+Alfonsojedi@users.noreply.github.com>
2024-10-21 14:23:04 +0200

    Update ProgressBar.jsx

--- 02-pokemon/src/components/ProgressBar.jsx
+++ 02-pokemon/src/components/ProgressBar.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 
 export function ProgressBar({amount,stat}){
-    
     const styleAmount = {
         width: eval(100*amount/255)+"%",
-        background: (amount<90 ? (amount<60 ? "#DD2200":"#DDAA00"):(amount<150 ?"#AADD00":"#22DD00"))
+        background: (amount<90 ? (amount<60 ? "#D20":"#DA0"):(amount<150 ?"#AD0":"#2D0"))
     }
     return(
         <div class="row mb-1 align">
             <div className='col-4 col-md-3 ministat'>{stat}</div>
-            <div class="col-8 col-md-9">
+            <div className="col-8 col-md-9">
                 <div className='progress trans' role="progressbar" aria-label={stat} aria-valuenow="100" aria-valuemin="0" aria-valuemax="255">
                     <div className='progress-bar bold' style={styleAmount}>{amount}</div>
                 </div>
