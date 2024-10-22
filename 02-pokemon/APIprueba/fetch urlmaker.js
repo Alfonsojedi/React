3fe2c1b059eb08341c7d43eadbca221101073f27
Alfonsojedi <150245030+Alfonsojedi@users.noreply.github.com>
2024-10-22 18:23:22 +0200

    Update fetch urlmaker.js

--- 02-pokemon/APIprueba/fetch urlmaker.js
+++ 02-pokemon/APIprueba/fetch urlmaker.js
@@ -3,6 +3,23 @@ async function Pokemon(url){//await only works on async functions
     let result = await response.json()//Convert the data into usable JSON
     console.log(result)//Print result
 }
+async function PokemonRegion(region){
+    //Saltar paso A si tienes el número
+    //region can be string and it works too
+    let response = await fetch("https://pokeapi.co/api/v2/"+region);//use the url to get data from the API
+    let result = await response.json()
+
+    //En gen 9,10 u 11+ este code daría error
+    response = await fetch(result.main_generation.url)
+    result = await response.json()
+    let pokeurls = [];
+    result.pokemon_especies.map(pokeobject => {
+        pokeurls.push(pokeobject.url)
+    })
+    return genobject
+    
+}
+/*
 function PokemonKanto(){
     for(let i=1; i<=151; i++){
         try{
@@ -12,8 +29,9 @@ function PokemonKanto(){
         }
     }
 }
+*/
 try{
-    PokemonKanto()
+    const urls = PokemonRegion("kanto")
 }catch(error){//Exception e in JavaScript
     console.error(error)
 }
