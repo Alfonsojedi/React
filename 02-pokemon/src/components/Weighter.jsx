8405abd6d89b9e11773ea9a3e834b08598c84ebf
Alfonsojedi <150245030+Alfonsojedi@users.noreply.github.com>
2024-11-05 18:05:10 +0100

    Update Weighter.jsx

--- 02-pokemon/src/components/Weighter.jsx
+++ 02-pokemon/src/components/Weighter.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export function Weighter ({height,weight}){
     return(
-        <div className='row aling center'>
+        <div className='row align center'>
             <div className="col-12 col-md-5 mb-1">Altura: <span className="kindatrans">{height/10}</span>m</div>
             <div className="col-12 col-md-5 mb-1">Peso: <span className="kindatrans">{weight/10}</span>kg</div>
         </div>
