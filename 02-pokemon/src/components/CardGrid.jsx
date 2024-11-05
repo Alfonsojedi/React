b1ad144d384694a82097478e1bd406f4b4d6c8ac
Alfonsojedi <150245030+Alfonsojedi@users.noreply.github.com>
2024-11-05 16:26:21 +0100

    Update CardGrid.jsx

--- 02-pokemon/src/components/CardGrid.jsx
+++ 02-pokemon/src/components/CardGrid.jsx
@@ -1,8 +1,7 @@
-//Usar este componente para reducir el App.jsx
 import { useState } from 'react'
 import PokeCard from './PokeCard'
 
-export function CardGrid ({pokemons}){
+export function CardGrid({pokemons}){
    return(
     <div className='row px-5 pt-1'>
         {pokemons.map(element => {
