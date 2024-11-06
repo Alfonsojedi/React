459b6e5ce791af2772d8ee0c68b6ec5939636e09
Alfonsojedi <150245030+Alfonsojedi@users.noreply.github.com>
2024-11-06 10:03:03 +0100

    Update CardGrid.jsx

--- 02-pokemon/src/components/CardGrid.jsx
+++ 02-pokemon/src/components/CardGrid.jsx
@@ -3,11 +3,11 @@ import PokeCard from './PokeCard'
 
 export function CardGrid({pokemons}){
    return(
-    <div className='row px-5 pt-1'>
-        {pokemons.map(element => {
+      <div className='row px-5 pt-1'>
+         {pokemons.map(element => {
             return <PokeCard pokemon={element}></PokeCard>
-        })}
-    </div>
+         })}
+      </div>
    )
 }
 
